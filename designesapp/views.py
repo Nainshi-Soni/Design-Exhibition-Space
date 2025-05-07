a49ac9b6b626628dc8d44e9bb82a6a5a8ee8f3a1
Samraddhigupta6388 <samraddhisatvik@gmail.com>
2025-05-07 11:49:27 +0530

    view updated

--- designesapp/views.py
+++ designesapp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return render(req,"index.html")
+def resume1(req):
+    return render(req,"resume1.html")
+def resume2(req):
+    return render(req,"resume2.html")
+def resume3(req):
+    return render(req,"resume3.html")
+def resume4(req):
+    return render(req,"resume4.html")
