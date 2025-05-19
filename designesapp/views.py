70a68333f15f37f0c41e1223ab45b65c71abf0e7
Samraddhigupta6388 <samraddhisatvik@gmail.com>
2025-05-19 22:36:28 +0530

    #

--- designesapp/views.py
+++ designesapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(req):
-    return render(req,"index.html")
 def resume1(req):
     return render(req,"resume1.html")
 def resume2(req):
@@ -11,3 +9,18 @@ def resume3(req):
     return render(req,"resume3.html")
 def resume4(req):
     return render(req,"resume4.html")
+def resume5(req):
+    return render(req,"resume5.html")
+
+def card6(req):
+    return render(req,"card6.html")
+def card5(req):
+    return render(req,"card5.html")
+def card4(req):
+    return render(req,"card4.html")
+def card3(req):
+    return render(req,"card3.html")
+def card2(req):
+    return render(req,"card2.html")
+def card1(req):
+    return render(req,"card1.html")
