83d83084b0618acc16cbe7d0e863374e12776649
ayushigupta-10 <ayushi1010gupta@gmail.com>
2025-05-19 19:25:15 +0530

    #

--- designesapp/models.py
+++ designesapp/models.py
@@ -5,4 +5,4 @@ class Portfolios(models.Model):
     port_id=models.IntegerField(primary_key=True)
     port_img=models.ImageField(null=True,upload_to="images/")
     port_link=models.CharField(max_length=50,null=True)
-    port_zip=models.FileField(null=True)
+    port_zip=models.FileField(null=True,upload_to="file/")
