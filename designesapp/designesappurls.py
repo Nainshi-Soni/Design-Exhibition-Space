6bcbf656f7a945c25f3dddd57c3d46534f7bc799
Samraddhigupta6388 <samraddhisatvik@gmail.com>
2025-05-22 00:12:41 +0530

    #

--- designesapp/designesappurls.py
+++ designesapp/designesappurls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns=[
-    path('resume1/',views.resume1,name='resume1'),
+    path('',views.resume1,name='resume1'),
     path('resume2/',views.resume2,name='resume2'),
     path('resume3/',views.resume3,name='resume3'),
     path('resume4/',views.resume4,name='resume4'),
